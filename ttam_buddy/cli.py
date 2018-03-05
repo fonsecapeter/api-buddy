e0f2fa8ba888bbdb19dfef25d475fb01a9d12bb9
Peter Fonseca <peter.nfonseca@gmail.com>
2018-03-05 10:22:08 -0800

    Resolve #4 Add Preferences

--- ttam_buddy/cli.py
+++ ttam_buddy/cli.py
@@ -1,14 +1,19 @@
 import click
-from .constants import VERSION
+from .constants import VERSION, PREFS_FILE
+from .preferences import load_prefs
 
 
 @click.command()
 @click.option('--version', is_flag=True, help='Show version and exit.')
 def run(version: bool) -> None:
-    """Explore the 23andMe API from your console"""
+    """Explore the 23andMe API from your terminal
+
+    Check out https://github.com/fonsecapeter/ttam-buddy for more info
+    """
     if version:
         click.echo(VERSION)
         return
+    load_prefs(PREFS_FILE)
     click.echo(':p')
 
 
--- ttam_buddy/constants.py
+++ ttam_buddy/constants.py
@@ -1,1 +1,5 @@
+from os import path
+
 VERSION = '0.1.0'
+PREFS_FILE = '~/.ttam-buddy.yml'
+ROOT_DIR = path.dirname(path.dirname(__file__))
