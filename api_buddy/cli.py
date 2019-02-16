71687ca25618da31904ad68f6d963ffdcba1489c
Peter Fonseca <peter.nfonseca@gmail.com>
2019-02-16 11:42:44 -0800

    Resolve #3 Use docopt

--- api_buddy/cli.py
+++ api_buddy/cli.py
@@ -1,24 +1,31 @@
-import click
+"""Explore OAuth2 APIs from your console
+
+Check out https://github.com/fonsecapeter/api-buddy for more info
+
+Usage:
+  api-buddy [options]
+
+Options:
+  -v, --version  Show installed version
+  -h, --help     Show this help message
+"""
+import json
+from docopt import docopt
 from urllib.parse import urljoin
 from .constants import VERSION, PREFS_FILE
 from .preferences import load_prefs
 from .session.oauth import get_oauth_session
 
 
-@click.command()
-@click.option('--version', is_flag=True, help='Show version and exit.')
-def run(version: bool) -> None:
-    """Explore the 23andMe API from your terminal
-
-    Check out https://github.com/fonsecapeter/api-buddy for more info
-    """
-    if version:
-        click.echo(VERSION)
+def run() -> None:
+    opts = docopt(__doc__)
+    if opts['--version']:
+        print(VERSION)
         return
     prefs = load_prefs(PREFS_FILE)
     sesh = get_oauth_session(prefs, PREFS_FILE)
     resp = sesh.get(urljoin(prefs['api_url'], '3/account'))
-    click.echo(resp.json())
+    print(json.dumps(resp.json(), indent=2))
 
 
 if __name__ == '__main__':
